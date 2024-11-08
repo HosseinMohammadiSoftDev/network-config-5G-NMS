3a7f61241377087b1663d215d9140d3ead202d10
Hoss MOHAMMADI <1386.hossein.mohammadi@gmail.com>
2024-11-08 21:38:47 +0330

    log implemeted

--- Modules/User/app/Http/Controllers/UserController.php
+++ Modules/User/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace Modules\User\Http\Controllers;
 use App\Http\Controllers\Contract\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Modules\User\Http\Requests\User\AddMemberRequest;
 use Modules\User\Http\Requests\User\resetPasswordRequest;
 use Modules\User\Models\User;
@@ -18,7 +19,7 @@ class UserController extends ApiController
         $this->paginationService = $paginationService;
     }
 
-    public function getMe (Request $request) 
+    public function getMe () 
     {
         $user = User::find(Auth::id());
 
@@ -59,20 +60,44 @@ class UserController extends ApiController
     public function addMember (AddMemberRequest $request)
     {
         $credentials = $request->validated();
-        $user = Auth::user();
-
         $credentials['added_by'] = Auth::id();
-        $user = User::create($credentials);
 
-        $role = $credentials['role'] ?? null;
+        try {
+                DB::beginTransaction();
+            $user = User::create($credentials);
+            $role = $credentials['role'] ?? null;
 
-        if ($role) 
-            $user->assignRole($role);
+            if ($role) 
+                $user->assignRole($role);
 
-        return $this->respondCreated('کاربر با موفقیت ساخته شد', [
-            'user' => $user,
-            'role' => $role
-        ]);
+                activity('add-member')
+                    ->causedBy(Auth::user())
+                    ->event('create-member')
+                    ->withProperties([
+                        'route' => request()->fullUrl(),
+                        'method' => 'addMember',
+                        'member' => $credentials,
+                    ])
+                ->log('ادمین کاربر را به برنامه ااضافه کرد'); 
+
+                DB::commit();
+            return $this->respondCreated('کاربر با موفقیت ساخته شد', ['user' => $user,'role' => $role ]);
+
+        } catch (\Exception $e) {
+                DB::rollBack();
+
+                activity('add-member')
+                    ->causedBy(Auth::user())
+                    ->event('create-member')
+                    ->withProperties([
+                        'route' => request()->fullUrl(),
+                        'method' => 'addMember',
+                        'member' => $credentials,
+                    ])
+                ->log('ادمین کاربر را به برنامه ااضافه کرد'); 
+            
+            return $this->respondInternalError('مشکلی در روند برنامه رخ داد');
+        }
     }
     public function resetPsswordAndAuthName (resetPasswordRequest $request)
     {
@@ -83,26 +108,61 @@ class UserController extends ApiController
         if ($user->hasRole('admin')) 
             return response()->json(['msg' => 'شما نمیتوانید کاربری که نقش ادمین را دارد را حذف کنید'], 403);
 
-        $user->update([
-           'auth_name' => $credentials['auth_name'] ?? $user['auth_name'],
-           'password' => $credentials['password'] ?? $user['password'],
-        ]);
+            
+        try {
+                DB::beginTransaction();    
+            
+            $user->update([
+                'auth_name' => $credentials['auth_name'] ?? $user['auth_name'],
+                'password' => $credentials['password'] ?? $user['password'],
+            ]);
+    
+                // کاربر را logout میکنیم
+            $user->tokens()->delete();
 
-            // کاربر را logout میکنیم
-        $user->tokens()->delete();
+            activity('reset-pass-and-auth-name')
+                ->causedBy(Auth::user())
+                ->performedOn($user)
+                ->event('update-pass-auth-name')
+                ->withProperties([
+                    'route' => request()->fullUrl(),
+                    'method' => 'resetPsswordAndAuthName',
+                    'user' => $user,
+                    'credentials' => $credentials
+                ])
+            ->log('نام  کاربری و پسورد کاربر توسط ادمین با موفقیت به روز رسانی شد');
 
-        return $this->respondSuccess('کاربر با موفقیت نمایش پیدا کرد', [
-            'user' => [
-                'id' => $user->id,
-                'first_name' => $user->first_name,
-                'last_name' => $user->last_name,
-                'auth_name' => $user->auth_name,
-                'created_at' => $user->created_at,
-                'updated_at' => $user->updated_at,
-                'roles' => $user->getRoleNames(),
-                'permissions' => $user->getAllPermissions()->pluck('name'),
-            ],
-        ]);
+                    DB::commit();
+            return $this->respondSuccess('نام کاربری و رمز عبور کاربر با موفقیت تعقییر کرد', [
+                'user' => [
+                    'id' => $user->id,
+                    'first_name' => $user->first_name,
+                    'last_name' => $user->last_name,
+                    'auth_name' => $user->auth_name,
+                    'created_at' => $user->created_at,
+                    'updated_at' => $user->updated_at,
+                    'roles' => $user->getRoleNames(),
+                    'permissions' => $user->getAllPermissions()->pluck('name'),
+                ],
+            ]);
+
+        } catch (\Exception $e) {
+                DB::rollBack();
+
+            activity('reset-pass-and-auth-name')
+                ->causedBy(Auth::user())
+                ->performedOn($user)
+                ->event('update-pass-auth-name')
+                ->withProperties([
+                    'route' => request()->fullUrl(),
+                    'method' => 'resetPsswordAndAuthName',
+                    'user' => $user,
+                    'error' => $e->getMessage()
+                ])
+            ->log('در تعویض نام کاربری و پسورد کاربر توسط ادمین مشکلی به وجود امد');
+
+            return $this->respondInternalError('مشکلی در به روز راسانی نام کاربری و پسورد پیش امده');
+        }          
     }
     public function deleteAccountMember ($userId)
     {
@@ -117,6 +177,18 @@ class UserController extends ApiController
         $user->delete();
 
         $user->tokens()->delete();
+
+        
+        activity('delete-account')
+            ->causedBy(Auth::user())
+            ->performedOn($user)
+            ->event('delete')
+            ->withProperties([
+                'route' => request()->fullUrl(),
+                'method' => 'deleteAccountMember',
+                'user' => $user,
+            ])
+        ->log('حساب کاربر با موفقیت توسط ادمین حذف شد');
 
         return $this->respondSuccess('اکانت کاربر با موفقیت حذف شد', $user);
     }
