b5891b794f2f031ebb23721d84d072f35436e9a9
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-06-01 15:49:03 +0330

    <change> [create server] : change bug create role as server.

--- Modules/Server/app/Http/Requests/Server/CreateServerRequest.php
+++ Modules/Server/app/Http/Requests/Server/CreateServerRequest.php
@@ -17,10 +17,6 @@ class CreateServerRequest extends FormRequest
 
             'path_config' => ['nullable', 'string',  'min:1', 'max:1024'],
             'path_run_config' => ['nullable', 'string',  'min:1', 'max:1024'],
-
-
-            'zabbix_ip' => ['nullable', 'string', 'max:255'],
-            'elk_ip' => ['nullable', 'string', 'max:255']
         ];
     }
 
