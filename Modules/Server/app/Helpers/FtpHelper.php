d98be08567d578837d4b7f14d8451831e19f42d4
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-06-25 01:21:44 +0330

    <feat> [delete file FTP] : delete file to FTPS method to FtpHelper implemmted.

--- Modules/Server/app/Helpers/FtpHelper.php
+++ Modules/Server/app/Helpers/FtpHelper.php
@@ -4,6 +4,7 @@ namespace Modules\Server\Helpers;
 
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Auth;
+use Modules\Server\Models\Module;
 use phpseclib3\Net\SSH2;
 
 class FtpHelper
@@ -70,7 +71,7 @@ class FtpHelper
             'localPath' => $localPath,
         ]);
     }
-    public function uploadFile (string $remotePath, string $fileContent, string $fileName): void
+    public function uploadFile (Module $module, string $fileContent): void
     {
         $sftp = new \phpseclib3\Net\SFTP($this->server['ip']);
 
@@ -82,24 +83,45 @@ class FtpHelper
         }
 
 
-        $fullPath = $remotePath . '/' . $fileName;
+        $fullPath = $module['path_config'] . $module['name'] . '.' . $module['extension'];
 
         $fileContent = $sftp->put($fullPath, $fileContent);
         if ($fileContent === false) {
-            $this->logActivity('sftp-upload-error', 'uploadFile', ['remotePath' => $remotePath]);
-            throw new \RuntimeException("Failed to upload remote file: $remotePath");
+            $this->logActivity('sftp-upload-error', 'uploadFile', ['remotePath' => $module['path_config']]);
+            throw new \RuntimeException("Failed to upload remote file: {$module['path_config']}");
         }
 
 
-        $dir = dirname($remotePath);
+        $dir = dirname($module['path_config']);
         if (!is_dir($dir))
             mkdir($dir, 0777, true);
 
 
 
         $this->logActivity('sftp-upload-success', 'uploadFile', [
-            'remotePath' => $remotePath,
-            'file_name' => $fileName
+            'remotePath' => $module['path_config'],
+            'file_name' => $module['name']
         ]);
+    }
+    public function deleteFile (Module $module): void
+    {
+        $sftp = new \phpseclib3\Net\SFTP($this->server['ip']);
+
+        if (!$sftp->login($this->username, $this->password)) {
+            $this->logActivity('failed-sftp-login', 'deleteFile');
+            throw new \Illuminate\Http\Exceptions\HttpResponseException(
+                response()->json(['msg' => 'SFTP login failed.'], 422)
+            );
+        }
+
+        $remotePath = $module['path_config'] . $module['name'] . '.' . $module['extension'];
+
+        $result = $sftp->delete($remotePath);
+        if ($result === false) {
+            $this->logActivity('sftp-delete-error', 'deleteFile', ['remotePath' => $remotePath]);
+            throw new \RuntimeException("Failed to delete remote file: $remotePath");
+        }
+
+        $this->logActivity('sftp-delete-success', 'deleteFile', ['remotePath' => $remotePath]);
     }
 }
