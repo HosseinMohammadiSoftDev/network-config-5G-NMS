926d7f286daaaa6e790fde40e1f1d4d76d71f5fc
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-07-09 15:47:12 +0330

    <del> [module_server] : deleted module_server table.

--- Modules/Server/database/migrations/2024_11_10_121601_create_modules_table.php
+++ Modules/Server/database/migrations/2024_11_10_121601_create_modules_table.php
@@ -14,11 +14,29 @@ return new class extends Migration
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
 
+            $table->boolean('is_updated')->default(false)
+                ->comment('change based on nms server module.');
+
+
             $table->string('name');
             $table->string('extension');
             $table->string('type');
 
             $table->string('path_config')->default('/home/siz-tel/bbdh-2.6.6-noCg/install/etc/bbdh/');
+
+
+
+            // config module
+//                                    json format
+            $table->text('initial_config_json')->nullable()->comment('initial config module to format son');
+            $table->text('previous_config_json')->nullable()->comment('previous config module to foram json');
+            $table->text('current_config_json')->nullable()->comment('config module');
+
+//                                    conf format
+            $table->text('initial_config_conf')->nullable()->comment('initial config module to foram conf');;
+            $table->text('previous_config_conf')->nullable()->comment('previous config module to foram conf');;
+
+
 
             $table->timestamps();
         });
