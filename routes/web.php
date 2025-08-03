072e4cf25e85b698513fc6c83192f5bcebd8f1ee
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-08-03 09:13:56 +0330

    <feat> [welcome page] : add welcome page implemmted

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,5 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
-    return response()->file(public_path('views/login.html'));
-});
-
-
-Route::get('/assets/{path}', function ($path) {
-    $filePath = resource_path("dist/assets/{$path}");
-    if (File::exists($filePath)) {
-        return Response::file($filePath);
-    }
-    abort(404);
-})->where('path', '.*');
-
-Route::get('recaptcha', function () {
-   return view('captcha');
+    return view('welcome');
 });
