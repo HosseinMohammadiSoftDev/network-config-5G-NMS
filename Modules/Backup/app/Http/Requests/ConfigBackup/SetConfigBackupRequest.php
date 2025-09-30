34fd5c83810e320bfb83107bc6d9ff171c62eae3
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-09-30 11:37:22 +0330

    <change> [directory validation] : destination path validator implemmted

--- Modules/Backup/app/Http/Requests/ConfigBackup/SetConfigBackupRequest.php
+++ Modules/Backup/app/Http/Requests/ConfigBackup/SetConfigBackupRequest.php
@@ -3,6 +3,7 @@
 namespace Modules\Backup\Http\Requests\ConfigBackup;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Modules\Backup\Utilities\DirectoryUtilities;
 
 class SetConfigBackupRequest extends FormRequest
 {
@@ -13,8 +14,37 @@ class SetConfigBackupRequest extends FormRequest
     {
         return [
             'password'         => ['required', 'string'],
-            'destination_path' => ['required', 'string'],
-            'run_backup_at'    => ['required', 'date_format:Y-m-d H:i:s', 'after:' . now()],
+            'destination_path' => ['required', 'string', 'regex:/^\/(?:[a-zA-Z0-9_\-\.]+\/)*[a-zA-Z0-9_\-\.]+\/$/',],
+            'run_backup_at'    => ['required', 'date_format:Y-m-d H:i:s', 'after:' . now()
+            ],
+        ];
+    }
+
+
+
+    public function withValidator ($validator) : void
+    {
+        if ($validator->errors()->any())
+            return;
+
+        $validator->after(function ($validator) {
+
+            DirectoryUtilities::basicValidator($this->input('destination_path'));
+            DirectoryUtilities::OwnerValidator($this->input('destination_path'));
+            DirectoryUtilities::PermissionValidator($this->input('destination_path', [700]));
+
+        });
+    }
+
+
+
+    public function messages () : array
+    {
+        return [
+            'destination_path.required' => 'The run config path is required.',
+            'destination_path.string' => 'The run config path must be a string.',
+            'destination_path.regex' => 'The run config path must be a valid absolute path ending with a slash (e.g., "/home/user/").',
+            'destination_path.not_regex' => 'The run config path cannot be a restricted system directory (e.g., "/bin/", "/etc/").',
         ];
     }
 
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
+use Modules\Backup\Models\BackupConfig;
+use Modules\Backup\Services\BackupService;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::call(function () {
+
+    $backupService = new BackupService();
+
+    $backupConfig = BackupConfig::query()
+        ->whereTime('run_backup_at', '>=', now())
+        ->first();
+
+
+    if ($backupConfig) $backupService->handle($backupConfig);
+
+})->everyMinute();
