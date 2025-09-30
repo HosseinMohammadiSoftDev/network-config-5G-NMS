18dca851efd10cfd62747f0f6d1a3f1aa4f71ec9
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-09-30 11:52:13 +0330

    <change> []

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,14 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
-
-
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -23,5 +25,16 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json(['message' => 'endpoin not found.'], 404);
+        });
+
+        $exceptions->renderable(function (MethodNotAllowedHttpException $e, $request) {
+            return response()->json(['message' => 'endpoin not found.'], 404);
+        });
+
+        $exceptions->renderable(function (Throwable $e, $request) {
+            if ($e instanceof RouteNotFoundException || $e instanceof AuthenticationException) return response()->json(['message' => 'Unauthenticated. Please provide a valid token.', 'success' => false], 401);
+            return null;
+        });
     })->create();
