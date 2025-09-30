e33610ac7ee3477ccd5f101f6a08a2f7b713efd6
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-09-30 11:53:10 +0330

    <del>  delete api test.

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,3 @@ use Illuminate\Http\Request;
 use Modules\User\Models\User;
 use Illuminate\Support\Facades\Route;
 
-
-
-Route::get('/test', function () {
-    User::create([
-        'first_name' => 'reza-2',
-        'last_name' => 'test',
-        'auth_name' => 'test/111',
-        'password' => 'password',
-    ]);
-
-    return response()->json(['msg' => 'مشکل از فرانت است.']);
-});
