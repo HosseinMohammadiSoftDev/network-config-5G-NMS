383df74eda6cc3c9290c6a35dc595b493b7a2592
Hossein Mohammadi <1386.hossein.mohammadi@gmail.com>
2025-10-03 22:37:52 +0330

    <feat> [console schedule] : add module schedule service to console schedule implemmted

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Log;
+use Modules\Server\Models\ModuleSchedule;
 use Illuminate\Support\Facades\Schedule;
 use Modules\Backup\Models\BackupConfig;
 use Modules\Backup\Services\BackupService;
+use Modules\Server\Service\Schedule\ModuleScheduleService;
 
 Schedule::call(function () {
 
-    $backupService = new BackupService();
+    $backupService  = new BackupService();
+    $moduleScheduleService = new ModuleScheduleService();
 
     $backupConfig = BackupConfig::query()
         ->whereTime('run_backup_at', '>=', now())
         ->first();
 
-
     if ($backupConfig) $backupService->handle($backupConfig);
+
+
+    $moduleSchedule = ModuleSchedule::query()
+        ->whereTime('run_scheduled_at', '>=', now())
+        ->find();
+
+    if ($moduleSchedule) $moduleScheduleService->handle($moduleSchedule);
 
 })->everyMinute();
